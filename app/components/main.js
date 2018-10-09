91adcdc5447e45ff098d84d74d6f88cd0b11f2e9
n.naryvkina <n.naryvkina@peppers-studio.ru>
2018-10-09 16:28:03 +0300

    текста в JSON\ событие main:ready

---
+++ app/components/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+	console.log("i'm ready!!");
+
+	/*
+	var o = {};
+	console.log( "$(o)'", $(o) );
+	console.log( "$('<canvas>')", $('<canvas>') );
+	console.log( "$('div')", $('div') );
+	*/
+
+	$.getJSON('assets/data.json', function(data) {
+		console.log(data);
+		$(window).trigger( 'main:ready', data );
+	});
+
+});
